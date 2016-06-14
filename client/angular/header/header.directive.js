8379283df1ec5df12ae8549e439b46f05a0fc610
Darlan Mendonça <darlanmendonca@gmail.com>
2016-06-14 12:01:24 -0300

    test header with bg

--- client/angular/header/header.directive.js
+++ client/angular/header/header.directive.js
@@ -5,30 +5,67 @@ angular
   .module('portfolium')
   .directive('header', HeaderDirective);
 
-function HeaderDirective($window) {
+function HeaderDirective() {
   return {
     restrict: 'C',
     link: link,
   };
 
-  function link(scope, header) {
+  function link() {
     let lastScrollTop = 0;
+    let header = document.querySelector('header');
 
     angular
-      .element($window)
+      .element(window)
       .bind('scroll', onScroll);
 
     function onScroll() {
-      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      let scrollDown = scrollTop > lastScrollTop;
+      let top = window.pageYOffset;
+      let scrollDown = top > lastScrollTop;
+
+      let bounding = header.getBoundingClientRect();
 
       if (scrollDown) {
-        header.addClass('hiden');
-      } else {
-        header.removeClass('hiden');
+        if (header.style.position !== 'absolute') {
+          header.style.position = 'absolute';
+          header.style.top = `${top - header.offsetTop}px`;
+        }
+      } else if (header.style.position === 'absolute' && bounding.bottom < 0){
+        header.style.position = 'absolute';
+        header.style.top = `${top - header.clientHeight}px`;
+      } else if (bounding.top >= 0) {
+        header.style.position = 'fixed';
+        header.style.top = '0px';
       }
 
-      lastScrollTop = scrollTop;
+      lastScrollTop = top;
     }
+
+    // setPositionHeader();
+
+    // function setPositionHeader(){
+    //   let top = window.pageYOffset;
+    //   let scrollDown = top > lastScrollTop;
+
+    //   let bounding = header.getBoundingClientRect();
+
+    //   if (scrollDown) {
+    //     if (header.style.position !== 'absolute') {
+    //       header.style.position = 'absolute';
+    //       header.style.top = `${top - header.offsetTop}px`;
+    //     }
+    //     element.addClass('hidden');
+    //   } else if (element.hasClass('hidden') && bounding.bottom < 0){
+    //     header.style.position = 'absolute';
+    //     header.style.top = `${top - header.clientHeight}px`;
+    //     element.removeClass('hidden');
+    //   } else if (bounding.top >= 0) {
+    //     header.style.position = 'fixed';
+    //     header.style.top = '0px';
+    //   }
+
+    //   lastScrollTop = top;
+    //   window.requestAnimationFrame(setPositionHeader);
+    // }
   }
 }
